fd8150a0834153ece5a0db676826481508e79984
lakshmipriyasevugaperumal <lpriyakausi@gmail.com>
2016-08-24 10:24:41 +0530

    Update pr4.java

--- pr4.java
+++ pr4.java
@@ -1,28 +1,25 @@
+public static int strToInt( String str ){
+    int i = 0;
+    int num = 0;
+    boolean isNeg = false;
 
-package com.java2novice.algos;
-public class MyBinarySearch {
-	public int binarySearch(int[] inputArr, int key) {
-		
-        int star = 0;
-        int end = inputArr.length - 1;
-        while (star <= end) {
-            int mid = (star + end) / 2;
-            if (key == inputArr[mid]) {
-                return mid;
-            }
-            if (key < inputArr[mid]) {
-            	end = mid - 1;
-            } else {
-            	star = mid + 1;
-            }
-        }
-        return -1;
+    //Check for negative sign; if it's there, set the isNeg flag
+    if (str.charAt(0) == '-') 
+{
+        isNeg = true;
+        i = 1;
     }
-    public static void main(String[] args) {
-    	MyBinarySearch mbs = new MyBinarySearch();
-    	int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
-    	System.out.println("Key 14's position: "+mbs.binarySearch(arr, 14));
-    	int[] arr1 = {6,34,78,123,432,900};
-    	System.out.println("Key 432's position: "+mbs.binarySearch(arr1, 432));
+
+    //Process each character of the string;
+    while( i < str.length()) 
+{
+        num *= 10;
+        num += str.charAt(i++) - '0';  //Minus the ASCII code of '0' to get the value of the charAt(i++).
     }
+
+    if (isNeg)
+{
+        num = -num;
+    return num;
+}
 }
