182fa1c5e05fa4d08129b8191ba85c770009e32f
lakshmipriyasevugaperumal <lpriyakausi@gmail.com>
2016-09-06 10:10:36 +0530

    Update p47.java

--- p47.java
+++ p47.java
@@ -1,17 +1,24 @@
+
 import java.util.Scanner;
 
-public class IntersectDot {
-	public static void main(String args[])
-	{
-		int k=0,d=0;
-		Scanner scr=new Scanner(System.in);
-		int n1=scr.nextInt();
-		int n2=scr.nextInt();
-		for(int i=1;i<=n1-1;i++)
-		   k+=i;
-		for(int j=1;j<=n2-1;j++)
-	          d+=k*j;
-		System.out.println(d);
+public class DotProblem {
+public static int dot(int upper,int lower){
+	int sum=0;
+int temp=0;
+for(int i=1;i<upper;i++)
+	temp+=i;
+for(int j=1;j<lower;j++)
+	sum+=temp*j;
+return sum;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scr=new Scanner(System.in);
+System.out.println("Enter number of dots at upper row");
+int upper=scr.nextInt();
+System.out.println("Enter the number of dots at lower row");
+int lower=scr.nextInt();
+System.out.println(dot(upper,lower));
 	}
 
 }
