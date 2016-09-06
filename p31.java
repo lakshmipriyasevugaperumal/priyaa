d0cd2a825746cabd190423feb60331f806a5302d
lakshmipriyasevugaperumal <lpriyakausi@gmail.com>
2016-09-06 09:21:06 +0530

    Update p31.java

--- p31.java
+++ p31.java
@@ -1,38 +1,62 @@
-private class PathHelper
-{
-    public int Value = 0;
-    public List<int> Path = new List<int>();
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DAG {
+public static void main(String[] args) {
+	Scanner scr=new Scanner(System.in); 
+	String s=scr.nextLine();//    3/\94/\/\182/\/\/\4582
+	ArrayList<Integer> al=new ArrayList<>();
+	ArrayList<Integer> al2=new ArrayList<>();
+	for(int i=0;i<s.length();i++)
+	{  if(Character.isDigit(s.charAt(i)))
+		{   
+			al.add(Integer.parseInt(s.charAt(i)+""));
+			
+		}
+		else
+		{
+		if(al.size()>1){
+		al2.add(check(al));
+		al=null;
+		al=new ArrayList<>();
+		}
+		else if(al.size()==1){
+			System.out.println("else");
+			al2.add(al.get(0));
+			al=null;
+			al=new ArrayList<>();
+			
+		}		
+			}
+		}
+	if(al.size()>1){
+		al2.add(check(al));
+		al=null;
+		al=new ArrayList<>();
+		}
+		else if(al.size()==1){
+			al2.add(al.get(0));
+			al=null;
+			al=new ArrayList<>();
+			
+		}
+	System.out.println(al2);
+	int sum=0;
+	for(int i:al2)
+	   sum+=i;
+	System.out.println("answer"+sum);
+	scr.close();
+	}
+	
+public static int check(ArrayList<Integer> al3)
+{  	int[] temp=new int[al3.size()];
+   int k=0;
+	for(int i:al3)
+		temp[k++]=i;
+	Arrays.sort(temp);
+		return temp[temp.length-1];
+		
 }
-
-public List<int> FindMaxPath(int[][] values)
-{
-    int n = values.Length;
-
-    PathHelper[] prev = new PathHelper[n];
-    PathHelper[] curr = null;
-    for (int i = 0; i < n; i++)
-    {
-        prev[i] = new PathHelper();
-        prev[i].Value = values[n - 1][i];
-        prev[i].Path.Add(values[n - 1][i]);
-    }
-
-    for (int i = n - 2; i >= 0; i--)
-    {
-        int m = values[i].Length;
-        curr = new PathHelper[m];
-
-        for (int j = 0; j < m; j++)
-        {
-            curr[j] = new PathHelper();
-            var max = (prev[j].Value > prev[j + 1].Value) ? prev[j] : prev[j + 1];
-            curr[j].Value = values[i][j] + max.Value;
-            curr[j].Path.Add(values[i][j]);
-            curr[j].Path.AddRange(max.Path);
-        }
-
-        prev = curr;
-    }
-
-    return curr[0].Path;
 }
